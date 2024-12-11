311358bab4c5f591edf475d2f5c99ec2ff299c57
Shashank S <shashank5418shashu@gmail.com>
2024-12-11 12:04:39 +0530

    Added Controllers

--- Routes/places-routes.js
+++ Routes/places-routes.js
@@ -1,74 +1,16 @@
 const exprees = require("express")
-const HttpError = require("../models/htttp-error")
+const placescontrollers = require("../controllers/places-controllers")
 
 const router = exprees.Router()
-
-const DUMMY_VALUES = [
-    {
-      id: "p1",
-      title: "Burj Khalifa",
-      imageURL:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8yfKNUZIfC9qe-Vz5SkVWSpPhDONel-ek-A&s",
-      address:
-        "1 Sheikh Mohammed bin Rashid Blvd - Downtown Dubai - Dubai - United Arab Emirates",
-      descrption:
-        "The Burj Khalifa is a skyscraper in Dubai, United Arab Emirates. It is the worlds tallest structure.",
-      location: {
-        lat: 25.197197,
-        long: 55.2743764,
-      },
-      creator: "u1",
-    },
-    {
-      id: "p2",
-      title: "Burj... Khalifa",
-      imageURL:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8yfKNUZIfC9qe-Vz5SkVWSpPhDONel-ek-A&s",
-      address:
-        "1 Sheikh Mohammed bin Rashid Blvd - Downtown Dubai - Dubai - United Arab Emirates",
-      descrption:
-        "The Burj Khalifa is a skyscraper in Dubai, United Arab Emirates. It is the worlds tallest structure.",
-      location: {
-        lat: 25.197197,
-        long: 55.2743764,
-      },
-      creator: "u2",
-    },
-  ];
 
 router.get("/" , (req,res,next)=>{
     console.log("This is data");
     res.json({message : "Hello Signup done"})
 })
 
-router.get("/:pid" , (req,res,next)=>{
-    const id = req.params.pid;
+router.get("/:pid" , placescontrollers.getPlacebyId)
 
-    const place = DUMMY_VALUES.find(p => id === p.id);
-
-
-    if(!place){
-        return next(new HttpError('Could not find the place with this placeId' , 404))
-    }
-
-
-    res.json({place})
-
-})
-
-router.get("/user/:uid" , (req,res,next)=>{
-    const uid = req.params.uid;
-
-    const User = DUMMY_VALUES.find(u=> uid === u.creator)
-
-    if(!User){
-        return next(new HttpError('Could not find the place with this UserId' , 404))
-    }
-
-
-    res.json({User}) //{User: User} Both are same
-
-})
+router.get("/user/:uid" , placescontrollers.getPlacebyUserId)
 
 
 module.exports = router
---
+++ controllers/places-controllers.js
@@ -0,0 +1,68 @@
+const HttpError = require("../models/htttp-error")
+const DUMMY_VALUES = [
+    {
+      id: "p1",
+      title: "Burj Khalifa",
+      imageURL:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8yfKNUZIfC9qe-Vz5SkVWSpPhDONel-ek-A&s",
+      address:
+        "1 Sheikh Mohammed bin Rashid Blvd - Downtown Dubai - Dubai - United Arab Emirates",
+      descrption:
+        "The Burj Khalifa is a skyscraper in Dubai, United Arab Emirates. It is the worlds tallest structure.",
+      location: {
+        lat: 25.197197,
+        long: 55.2743764,
+      },
+      creator: "u1",
+    },
+    {
+      id: "p2",
+      title: "Burj... Khalifa",
+      imageURL:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8yfKNUZIfC9qe-Vz5SkVWSpPhDONel-ek-A&s",
+      address:
+        "1 Sheikh Mohammed bin Rashid Blvd - Downtown Dubai - Dubai - United Arab Emirates",
+      descrption:
+        "The Burj Khalifa is a skyscraper in Dubai, United Arab Emirates. It is the worlds tallest structure.",
+      location: {
+        lat: 25.197197,
+        long: 55.2743764,
+      },
+      creator: "u2",
+    },
+  ];
+
+
+
+
+const getPlacebyId = (req,res,next)=>{
+    const id = req.params.pid;
+
+    const place = DUMMY_VALUES.find(p => id === p.id);
+
+
+    if(!place){
+        return next(new HttpError('Could not find the place with this placeId' , 404))
+    }
+
+
+    res.json({place})
+
+}
+
+const getPlacebyUserId = (req,res,next)=>{
+    const uid = req.params.uid;
+
+    const User = DUMMY_VALUES.find(u=> uid === u.creator)
+
+    if(!User){
+        return next(new HttpError('Could not find the place with this UserId' , 404))
+    }
+
+
+    res.json({User}) 
+
+}
+
+exports.getPlacebyId = getPlacebyId;
+exports.getPlacebyUserId = getPlacebyUserId;
