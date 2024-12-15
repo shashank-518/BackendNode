441c3c679a43b2f6737c4b0c58c49573f2035cda
Shashank S <shashank5418shashu@gmail.com>
2024-12-15 11:49:37 +0530

    Changes made

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-
+const HttpError = require("./models/htttp-error")
 const PlacesRoute = require("./Routes/places-routes")
 
 
@@ -10,6 +10,11 @@ const app = express()
 app.use(bodyParser.json())
 
 app.use('/api/places' , PlacesRoute)
+
+app.use((req,res)=>{
+    const Error =new  HttpError("No Route defined" , 404)
+    throw(Error)
+})
 
 
 app.use((error,req,res,next)=>{
